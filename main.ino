c0518ddfe76d4aa494a6cde4da058f1d542cf59a
Chenmengdan <34216694+chenmengdan@users.noreply.github.com>
2018-03-12 20:24:31 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -7,13 +7,10 @@
 #include <Stepper.h>
 
 /* ****************** WINDOW SHADE PART ******************************** */
-Stepper stepper(STEPS,8,10,9,11);  
+Stepper stepper(STEPS,15,16,17,18);  
 const int thresholdvalue = 120;         //The treshold for which the LED should turn on. Setting it lower will make it go on at more light, higher for more darkness
 
 #define LIGHT_SENSOR A0                 //Grove - Light Sensor is connected to A0 of Arduino
-#define MANUAL_PIN 2
-#define AUTO_MODE_PIN 3
-#define LED_PIN 12           //Connect the LED Grove module to Pin12, Digital 12
 #define STEPS 2038
 /* ********************************************************************** */
 
@@ -39,12 +36,14 @@ decode_results results;
 
 int pressedNumber = 0;
 int count = 0;
-bool room1 = false;
-bool room2 = false;
-bool room3 = false;
 
-bool childBedRoomState = true;
-bool childAptState = true;
+
+
+bool IS_CHILD_IN_KITCHEN = false;
+bool IS_CHILD_IN_BATHROOM = false;
+
+bool IS_PARENT_IN_KITCHEN = false;
+bool IS_PARENT_IN_BATHROOM = false;
 
 
 /* ****************** WINDOW SHADE PART ******************************** */
@@ -63,11 +62,12 @@ void setup() {
   pinMode(TouchPin, INPUT);
 
 /* ****************** WINDOW SHADE PART ******************************** */
-  pinMode(LED_PIN,OUTPUT);            //Set the LED on Digital 12 as an OUTPUT
-  pinMode(MANUAL_PIN, INPUT);  // initialize the pushbutton pin as an input
-  pinMode(AUTO_MODE_PIN, INPUT); // initialize the pushbutton pin as an input
-  attachInterrupt(digitalPinToInterrupt(receiver), manualButtonController, RISING);
-  attachInterrupt(digitalPinToInterrupt(receiver), lightSensorMode, RISING);
+//  pinMode(LED_PIN,OUTPUT);            //Set the LED on Digital 12 as an OUTPUT
+//  pinMode(MANUAL_PIN, INPUT);  // initialize the pushbutton pin as an input
+//  pinMode(AUTO_MODE_PIN, INPUT); // initialize the pushbutton pin as an input
+//  attachInterrupt(digitalPinToInterrupt(receiver), manualButtonController, RISING);
+//  attachInterrupt(digitalPinToInterrupt(receiver), lightSensorMode, RISING);
+  attachInterrupt(0,translateIR,CHANGE);
   stepper.setSpeed(15);
 /* ********************************************************************** */
 
@@ -83,110 +83,43 @@ void setup() {
 }
 
 void loop() {
-  if (irrecv.decode(&results)) {// have we received an IR signal?
-    translateIR();
-    irrecv.resume();//receive the next value
-  }
-
-  /* **************************RFID 1 ***************************** */
-
-    Serial.print("UID tag :");
-    String content= "";
-    byte letter;
-    for (byte i = 0; i < mfrc522.uid.size; i++)
-    {
-       Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-       Serial.print(mfrc522.uid.uidByte[i], HEX);
-       content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-       content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    if (irrecv.decode(&results)) {// have we received an IR signal?
+      translateIR();
+      irrecv.resume();//receive the next value
     }
-    Serial.println();
-    Serial.print("Message : ");
-    content.toUpperCase();
-   
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()){
+    checkLocation();
 
-      if (content.substring(1) == "06 D4 37 07") //Child
-      {
-        Serial.println("Child");
-        Serial.println();
-        delay(1000);
-      }
+ /* ************************* CALLING PARENTS ********************************* */
+    if( pressedNumber == 1 )
+    {
+        if(IS_PARENT_IN_KITCHEN == true)
+        {
+            buzzerLEDActivate();
+        }
+        else if(IS_PARENT_IN_BATHROOM == true)
+        {
+            buzzerLEDActivate();
+            lcd.setRGB(255,255,255);
+        }
+        else // PARENT IN THE BED ROOM
+        {
+            if(digitalRead(TouchPin) == 1) 
+            {
+              Serial.println("In Bed -> Vibrate, buzzer, LED");
+              Serial.println();
+              vibrateBuzzerLEDActivate(); 
+            }
+        } 
+        pressedNumber = 0;
+    }
+
+
+
+      
+      
+    }
     
-      else if (content.substring(1) == "D1 96 AF 85") // Parent 
-      {
-        Serial.println("Parent");
-        delay(1000);
-      }
-      
-      if (childBedRoomState == true)
-      {
-        Serial.println("Child is going out of the room");
-        lcd.begin(16, 2);  
-        lcd.setCursor(0, 0);// Print a message to the lcd.
-        lcd.print("Child gng out bedrm ");
-        lcd.setRGB(254,0,0);
-      }
-      else
-      {
-        Serial.println("Child is inside the room");
-        lcd.begin(16, 2);  
-        lcd.setCursor(0, 0);// Print a message to the lcd.
-        lcd.print("Child gng in bedrm ");
-        lcd.setRGB(254,0,0);
-      }
-      childBedRoomState = !childBedRoomState;
-      delay(1000);
-  }
-  
-/* *****************************RFID 2 ************************************* */
-    Serial.print("UID tag :");
-    String content2= "";
-    byte letter2;
-    for (byte i = 0; i < mfrc522_2.uid.size; i++)
-    {
-       Serial.print(mfrc522_2.uid.uidByte[i] < 0x10 ? " 0" : " ");
-       Serial.print(mfrc522_2.uid.uidByte[i], HEX);
-       content2.concat(String(mfrc522_2.uid.uidByte[i] < 0x10 ? " 0" : " "));
-       content2.concat(String(mfrc522_2.uid.uidByte[i], HEX));
-    }
-    Serial.println();
-    Serial.print("Message : ");
-    content2.toUpperCase();
 
-   
-   if (mfrc522_2.PICC_IsNewCardPresent() && mfrc522_2.PICC_ReadCardSerial()){
-
-      if (content2.substring(1) == "06 D4 37 07") //Child
-      {
-        Serial.println("Child");
-        Serial.println();
-        delay(1000);
-      }
-      else if (content2.substring(1) == "D1 96 AF 85") //Parent
-      {
-        Serial.println("Parent");
-        delay(1000);
-      }
-      
-      if (childAptState == true)
-      {
-        Serial.println("Child is going out of the apt");
-        lcd.begin(16, 2);  
-        lcd.setCursor(0, 0);// Print a message to the lcd.
-        lcd.print("Child gng out apt ");
-        lcd.setRGB(254,0,0);
-      }
-      else{
-        Serial.println("Child is inside the apt");
-        lcd.begin(16, 2);  
-        lcd.setCursor(0, 0);// Print a message to the lcd.
-        lcd.print("Child gng in apt ");
-        lcd.setRGB(254,0,0);
-      }
-      childAptState = !childAptState;
-      delay(1000);
-  }
   
 /* ************************** WINDOW SHADE PART  ****************************** */
 //    buttonValueOfMannaul = digitalRead(MANUAL_PIN);
@@ -228,21 +161,12 @@ void translateIR() {// takes action based on IR code received
   switch(results.value)
   { 
     case 16753245:
+      pressedNumber = 1;
       Serial.println("Button_1 - Child Calling!!");
       lcd.begin(16, 2);  
       lcd.setCursor(0, 0);// Print a message to the lcd.
       lcd.print("Child is Calling! ");
       lcd.setRGB(254,0,0);
-
-      if(digitalRead(TouchPin) == 1) {
-        Serial.println("In Bed -> Vibrate, buzzer, LED");
-        Serial.println();
-        vibrateBuzzerLEDActivate(); 
-      }
-      else {
-        buzzerLEDActivate();
-        lcd.setRGB(255,255,255);
-      } 
       break;
 //    case 16736925:
 //        Serial.println("Button_2 - Light ON/OFF");
@@ -254,22 +178,24 @@ void translateIR() {// takes action based on IR code received
       lcd.clear();
       lcd.setRGB(0,0,0);   
       break;
-//      case 16769565:
-//        Serial.println("3");
-//        pressedNumber = 3;
-//        lcd.begin(16, 2);  
-//        lcd.setCursor(0, 0);// Print a message to the lcd.
-//        lcd.print("Child is Calling! ");
-//        lcd.setRGB(254,0,0);     
-//        break;
-//      case 16720605:
-//        Serial.println("4");
-//        pressedNumber = 4;
-//        lcd.begin(16, 2);  
-//        lcd.setCursor(0, 0);// Print a message to the lcd.
-//        lcd.print("Open the Light ");
-//        lcd.setRGB(254,150,0);  
-//        break;
+      case 16769565:
+        Serial.println("3");
+        pressedNumber = 3;
+        lcd.begin(16, 2);  
+        lcd.setCursor(1, 0);// Print a message to the lcd.
+        lcd.print("Manual Mode Window");
+        lcd.setRGB(66,244,104);
+        manualButtonController();
+        break;
+      case 16720605:
+        Serial.println("4");
+        pressedNumber = 4;
+        lcd.begin(16, 2);  
+        lcd.setCursor(0, 0);// Print a message to the lcd.
+        lcd.print("Auto Mode Window");
+        lcd.setRGB(176,66,244);
+        lightSensorMode();
+        break;
 //      case 16712445:
 //        Serial.println("5");
 //        pressedNumber = 5;    
@@ -311,6 +237,140 @@ void translateIR() {// takes action based on IR code received
 //        Serial.println(" REPEAT"); break;
     }// End Case
   delay(50); // Do not get immediate repeat
+}
+
+void checkLocation(){
+   /* **************************RFID 1 ***************************** */
+        Serial.print("UID tag :");
+        String content= "";
+        byte letter;
+        for (byte i = 0; i < mfrc522.uid.size; i++)
+        {
+           Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+           Serial.print(mfrc522.uid.uidByte[i], HEX);
+           content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+           content.concat(String(mfrc522.uid.uidByte[i], HEX));
+        }
+        Serial.println();
+        Serial.print("Message : ");
+        content.toUpperCase();
+
+   /* ****************************** RFID AT KITCHEN ***************************************** */ 
+      if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()){
+    
+          if (content.substring(1) == "06 D4 37 07") //Child
+          { 
+              Serial.println("Child");
+              IS_CHILD_IN_KITCHEN = !IS_CHILD_IN_KITCHEN;
+             
+              if (IS_CHILD_IN_KITCHEN == true)
+              {
+                Serial.println("CHILD IS IN THE KITCHEN NOW");
+                lcd.begin(16, 2);  
+                lcd.setCursor(0, 0);// Print a message to the lcd.
+                lcd.print("CHILD IS IN THE KITCHEN NOW!!");
+                lcd.setRGB(254,0,0);
+              }
+              else if (IS_CHILD_IN_KITCHEN == false)
+              {
+                Serial.println("CHILD IS NOT IN THE KITCHEN NOW");
+                lcd.begin(16, 2);  
+                lcd.setCursor(0, 0);// Print a message to the lcd.
+                lcd.print("CHILD IS NOT IN THE KITCHEN NOW!!");
+                lcd.setRGB(254,0,0);
+              } 
+              delay(1000);
+          }
+            
+          else if (content.substring(1) == "D1 96 AF 85") // Parent 
+          {
+            Serial.println("Parent"); 
+            IS_PARENT_IN_KITCHEN = !IS_PARENT_IN_KITCHEN;
+            
+            if (IS_PARENT_IN_KITCHEN == true)
+              {
+                Serial.println("PARENT IS IN THE KITCHEN NOW");
+                lcd.begin(16, 2);  
+                lcd.setCursor(0, 0);// Print a message to the lcd.
+                lcd.print("PARENT IS IN THE KITCHEN NOW!!");
+                lcd.setRGB(254,0,0);
+              }
+              else if (IS_PARENT_IN_KITCHEN == false)
+              {
+                Serial.println("PARENT IS NOT IN THE KITCHEN NOW");
+                lcd.begin(16, 2);  
+                lcd.setCursor(0, 0);// Print a message to the lcd.
+                lcd.print("PARENT IS NOT IN THE KITCHEN NOW!!");
+                lcd.setRGB(254,0,0);
+              } 
+            delay(1000);
+          }
+      }
+      
+    /* ***************************** RFID 2 ************************************* */
+        Serial.print("UID tag :");
+        String content2= "";
+        byte letter2;
+        for (byte i = 0; i < mfrc522_2.uid.size; i++)
+        {
+           Serial.print(mfrc522_2.uid.uidByte[i] < 0x10 ? " 0" : " ");
+           Serial.print(mfrc522_2.uid.uidByte[i], HEX);
+           content2.concat(String(mfrc522_2.uid.uidByte[i] < 0x10 ? " 0" : " "));
+           content2.concat(String(mfrc522_2.uid.uidByte[i], HEX));
+        }
+        Serial.println();
+        Serial.print("Message : ");
+        content2.toUpperCase();
+    
+   /* ****************************** RFID AT BATHROOM ******************************** */
+       if (mfrc522_2.PICC_IsNewCardPresent() && mfrc522_2.PICC_ReadCardSerial()){
+        
+         
+          if (content2.substring(1) == "06 D4 37 07") //Child
+          {
+            Serial.println("Child");
+            IS_CHILD_IN_BATHROOM = !IS_CHILD_IN_BATHROOM;
+            
+            if (IS_CHILD_IN_BATHROOM == true)
+            {
+              Serial.println("CHILD IS IN THE BATHROOM NOW");
+              lcd.begin(16, 2);  
+              lcd.setCursor(0, 0);// Print a message to the lcd.
+              lcd.print("CHILD IS IN THE BATHROOM NOW!!!");
+              lcd.setRGB(254,0,0);
+            }
+            else if (IS_CHILD_IN_BATHROOM == false){
+              Serial.println("CHILD IS NOT IN THE BATHROOM NOW");
+              lcd.begin(16, 2);  
+              lcd.setCursor(0, 0);// Print a message to the lcd.
+              lcd.print("CHILD IS NOT IN THE BATHROOM NOW!!!");
+              lcd.setRGB(254,0,0);
+            }
+            delay(1000);
+          }
+          
+          else if (content2.substring(1) == "D1 96 AF 85") //Parent
+          {
+              Serial.println("Parent");
+              IS_PARENT_IN_BATHROOM = !IS_PARENT_IN_BATHROOM;
+              if (IS_PARENT_IN_BATHROOM == true)
+              {
+                Serial.println("CHILD IS IN THE BATHROOM NOW");
+                lcd.begin(16, 2);  
+                lcd.setCursor(0, 0);// Print a message to the lcd.
+                lcd.print("CHILD IS IN THE BATHROOM NOW!!!");
+                lcd.setRGB(254,0,0);
+              }
+              else if (IS_PARENT_IN_BATHROOM == false){
+                Serial.println("PARENT IS NOT IN THE BATHROOM NOW");
+                lcd.begin(16, 2);  
+                lcd.setCursor(0, 0);// Print a message to the lcd.
+                lcd.print("PARENT IS NOT IN THE BATHROOM NOW!!!");
+                lcd.setRGB(254,0,0);
+              }
+              delay(1000);
+           }    
+        }
 }
 
 
